dadecfcb488131a6bbf43de3fe09957b77cd92a0
vladrebe <35567934+vladrebe@users.noreply.github.com>
2018-05-24 20:12:23 +0300

    Add files via upload

--- iframe.js
+++ iframe.js
@@ -1,12 +1,14 @@
-var numbers = [ 
-2, 7, 19, 1, 20, 8, 14, 21, 13, 8, 3, 8, 90, 1, 91, 18, 61, 27, 60, 19, 43, 91, 
-] 
-var last_index = 0 
+var numbers = [
+345,
+211,
+428
+]
+var last_index = 0
 
-function getTrueRandomInteger(min,max){ 
-document.getElementById("true-random-integer-generator-result").innerHTML='<img src="/util/cp/images/ajax-loader.gif" alt="Loading..." />'; 
-setTimeout(function(){ 
-document.getElementById("true-random-integer-generator-result").innerHTML=numbers[last_index]; 
-last_index += 1; 
-}, 700); 
+function getTrueRandomInteger(min,max){
+	document.getElementById("true-random-integer-generator-result").innerHTML='<img src="/util/cp/images/ajax-loader.gif" alt="Loading..." />';
+	setTimeout(function(){
+		document.getElementById("true-random-integer-generator-result").innerHTML=numbers[last_index];
+		last_index += 1;
+	}, 700); 
 }
